854816f3f1882431a24f3044c87514f051944802
Austin Limanek <austin.limanek@gmail.com>
2022-10-28 16:37:45 -0700

    initialized and started coding

--- java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -1,7 +1,14 @@
 package datastructures.linkedlist;
 
+public class Node
+{
+  String value;
+  Node next;
+}
+
 public class LinkedList
 {
+  Node head;
   @Override
   public String toString()
     {
