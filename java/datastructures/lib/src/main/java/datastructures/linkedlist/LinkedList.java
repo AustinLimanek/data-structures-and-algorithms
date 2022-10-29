8ff77c9b7ca349bb99c9fd70e13868e28ac9bc56
Austin Limanek <austin.limanek@gmail.com>
2022-10-28 22:46:30 -0700

    finished the remaining tests

--- java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -31,16 +31,28 @@ public class LinkedList
     return string + "Null";
   }
 
+  public void endAppend(String value){
+    Node current = this.head;
+    while(current.next != null){
+      current = current.next;
+    }
+    current.next = new Node();
+    current.next.value = value;
+  }
+
   public static void main(String[] args){
     LinkedList linkedList = new LinkedList();
-    linkedList.insert( "hello");
     linkedList.insert( "world");
+    linkedList.insert( "hello");
 
     System.out.println(linkedList.head.value);
     System.out.println(linkedList.head.next.value);
     System.out.println(linkedList.includes("hi"));
     System.out.println(linkedList.toAString());
+    linkedList.endAppend("to you");
+    System.out.println(linkedList.toAString());
   }
+
 }
 
 
--- java/datastructures/lib/src/test/java/datastructures/linkedlist/LinkedListTest.java
+++ java/datastructures/lib/src/test/java/datastructures/linkedlist/LinkedListTest.java
@@ -6,12 +6,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class LinkedListTest
 {
+  //instantiate an empty linked list
   @Test void linkedListExists()
     {
       LinkedList sut = new LinkedList();
       assertNull(sut.head);
     }
 
+  //can properly insert into a linked list
+  // head property will properly point to the first node in the linked list
+  //can properly insert multiple nodes into the linked list
   @Test void insertIntoList()
   {
     LinkedList sut = new LinkedList();
@@ -19,6 +23,26 @@ public class LinkedListTest
     sut.insert("hello");
     assertEquals("hello", sut.head.value);
     assertEquals("world", sut.head.next.value);
+  }
+
+  //Will return true when finding a value within the linked list that exists
+  //Will return false when not finding a value within the linked list
+  @Test void testIncludes()
+  {
+    LinkedList sut = new LinkedList();
+    sut.insert("world");
+    sut.insert("hello");
+    assertTrue(sut.includes("world"));
+    assertFalse(sut.includes("austin"));
+  }
+
+  //Can properly return a collection of all the values that exist in the linked list
+  @Test void testToAString()
+  {
+    LinkedList sut = new LinkedList();
+    sut.insert("world");
+    sut.insert("hello");
+    assertEquals("{ hello } -> { world } -> Null", sut.toAString());
   }
 
 
