2ccb3460caa43c2880551b83ab2bf3e086a8c2d3
Austin Limanek <austin.limanek@gmail.com>
2022-11-07 23:51:40 -0800

    added all of the required functionality

---
+++ java/datastructures/lib/src/main/java/codechallenges/stackqueue/PseudoQueue.java
@@ -0,0 +1,36 @@
+package codechallenges.stackqueue;
+
+import datastructures.linkedlist.Node;
+import datastructures.stack.Stack;
+
+public class PseudoQueue<T> {
+
+public Stack<T> left = new Stack<>();
+private Stack<T> right = new Stack<>();
+
+public Node<T> dequeue(){
+  if(left.isEmpty()) return null;
+  Node<T> tempNode;
+  tempNode = left.pop();
+  return tempNode;
+}
+
+public void enqueue(T value){
+  if(left.isEmpty()) left.push(value);
+  else {
+    Node<T> tempNode;
+    while (!left.isEmpty()) {
+      tempNode = left.pop();
+      right.push(tempNode.value);
+    }
+    left.push(value);
+    while (!right.isEmpty()) {
+      tempNode = right.pop();
+      left.push(tempNode.value);
+    }
+  }
+}
+
+
+
+}
---
+++ java/datastructures/lib/src/test/java/codechallenges/stackqueue/PseudoQueueTest.java
@@ -0,0 +1,45 @@
+package codechallenges.stackqueue;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PseudoQueueTest {
+
+  @Test void dequeueEmpty(){
+    PseudoQueue<String> test = new PseudoQueue<>();
+    assertNull(test.dequeue());
+  }
+
+  @Test void enqueueToEmpty(){
+    PseudoQueue<String> test = new PseudoQueue<>();
+    test.enqueue("hello");
+    assertEquals("hello", test.left.top.value);
+  }
+
+  @Test void dequeueSinglePseudoQueue(){
+    PseudoQueue<String> test = new PseudoQueue<>();
+    test.enqueue("hello");
+    test.dequeue();
+    assertTrue(test.left.isEmpty());
+  }
+
+  @Test void enqueueManyPseudoQueue(){
+    PseudoQueue<String> test = new PseudoQueue<>();
+    test.enqueue("hello");
+    test.enqueue("to");
+    test.enqueue("you");
+    test.enqueue("world");
+    assertEquals("world", test.left.top.next.next.next.value);
+  }
+
+  @Test void dequeueFromManyPseudoQueue(){
+    PseudoQueue<String> test = new PseudoQueue<>();
+    test.enqueue("hello");
+    test.enqueue("to");
+    test.enqueue("you");
+    test.dequeue();
+    assertEquals("to", test.left.peek());
+  }
+
+}
