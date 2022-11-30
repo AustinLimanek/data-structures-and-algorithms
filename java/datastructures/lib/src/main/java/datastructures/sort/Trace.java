3d8f17876bb95897a0fca040d133a661bb9ea61e
Austin Limanek <austin.limanek@gmail.com>
2022-11-29 19:16:20 -0800

    added stuff to mergeSort

--- java/datastructures/lib/src/main/java/datastructures/sort/Trace.java
+++ java/datastructures/lib/src/main/java/datastructures/sort/Trace.java
@@ -21,4 +21,52 @@ public class Trace {
     }
     return array;
   }
+
+  public void mergeSortArray(ArrayList<Integer> array){
+    int size = array.size();
+
+    if (size > 1){
+
+      int mid = size/2;
+      ArrayList<Integer> left = new ArrayList<>(array.subList(0,mid));
+      ArrayList<Integer> right = new ArrayList<>(array.subList(mid,size));
+
+      mergeSortArray(left);
+      mergeSortArray(right);
+      mergeArrays(left, right, array);
+    }
+
+  }
+
+  public void mergeArrays(ArrayList<Integer> left, ArrayList<Integer> right, ArrayList<Integer> array){
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    while (i < left.size() && j < right.size()){
+
+      if (left.get(i) < right.get(j)){
+        array.set(k, left.get(i));
+        i++;
+      }
+      else{
+        array.set(k, right.get(j));
+        j++;
+      }
+      k++;
+    }
+
+    if (i == left.size()){
+      for (int x = j; x < right.size(); x++){
+        array.set(k, right.get(x));
+        k++;
+      }
+    }
+    else{
+      for (int x = i; x < left.size(); x++){
+        array.set(k, left.get(x));
+        k++;
+      }
+    }
+  }
 }
